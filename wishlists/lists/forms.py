8d1f6f66ada450fa49731d2e3cdfb685aa30546f
Richard Paul <richardnpaul@gmail.com>
2018-09-11 20:05:49 +0100

    Lots of work on the modelform for the wishlist and templates and models etc.  No tests.  Just progress towards the final idea for this site.

--- wishlists/lists/forms.py
+++ wishlists/lists/forms.py
@@ -7,21 +7,38 @@ from .models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty wishlist item"
 
 
-class ItemForm(forms.models.ModelForm):
+class ItemForm(forms.ModelForm):
+
+    url = forms.URLField(required=False, widget=forms.URLInput(
+        attrs={
+            'placeholder': 'Enter a the website address to a wishlist item here',
+            'class': 'form-control',
+        }))
+    price = forms.DecimalField(required=False, widget=forms.NumberInput(
+        attrs={'class': 'form-control'}))
+    number = forms.DecimalField(initial=1, widget=forms.NumberInput(
+        attrs={'class': 'form-control'}))
 
     class Meta:
         model = Item
-        fields = ('text',)
+        fields = ('text', 'url', 'price', 'number', 'priority')
         widgets = {
-            'text': forms.fields.TextInput(
+            'text': forms.TextInput(
                 attrs={
                     'placeholder': 'Enter a wishlist item here',
-                    'class': 'form-control input-lg',
+                    'class': 'form-control',
                 }
             ),
+            'priority': forms.Select(
+                attrs={
+                    'class': 'form-check-input '
+                }
+            )
         }
         error_messages = {
-            'text': {'required': EMPTY_ITEM_ERROR}
+            'text': {'required': EMPTY_ITEM_ERROR},
+            'number': {'required': 'A desired number needs to be selected. Defaults to 1'},
+            'priority': {'required': 'A priority must be submitted. Defaults to medium'},
         }
 
     def save(self, for_list):
--- wishlists/lists/models.py
+++ wishlists/lists/models.py
@@ -7,6 +7,9 @@ from django.urls import reverse
 
 
 class Wishlist(models.Model):
+    # name = models.CharField(default=None)
+    # private = models.BooleanField(default=True)
+    # owner = models.OneToOneField()
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False,
                             db_index=True, max_length=32)
 
@@ -15,6 +18,22 @@ class Wishlist(models.Model):
 
 
 class Item(models.Model):
+    PRIORITY_CHOICES = (
+        ('highest', 'Highest'),
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+        ('lowest', 'Lowest'),
+    )
+
     text = models.TextField()
+    url = models.URLField()
+    price = models.DecimalField(null=True, blank=True, decimal_places=2,
+                                max_digits=16)
+    number = models.IntegerField(default=1)
+    priority = models.CharField(default='medium', choices=PRIORITY_CHOICES,
+                                max_length=8)
+    # notes = models.TextField()
+    # date_added = models.DateTimeField()
     wishlist = models.ForeignKey(Wishlist, default=None,
                                  on_delete=models.CASCADE)
