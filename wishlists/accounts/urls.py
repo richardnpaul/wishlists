92ee4c8a34ba42382c90ce9b1c0369a3991742d7
Richard Paul <richardnpaul@gmail.com>
2018-10-14 19:39:09 +0100

    Account password reset and open links in new tabs

--- wishlists/accounts/urls.py
+++ wishlists/accounts/urls.py
@@ -1,5 +1,6 @@
 # Django
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 # Local
 from . import views
@@ -9,5 +10,9 @@ urlpatterns = [
     path('logout/', views.account_logout, name='account_logout'),
     path('register/', views.account_registration, name='account_registration'),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetView.as_view(), name='password_reset_done'),
+    path('reset/<str:uidb64>/<str:token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('social_django.urls', namespace='social'))
 ]
