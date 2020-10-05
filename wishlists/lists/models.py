877628ba403eab11ac1e11e4d81bc9682482f2c1
Richard Paul <richard@richardnpaul.co.uk>
2020-10-05 19:47:58 +0100

    isort changes

--- wishlists/lists/models.py
+++ wishlists/lists/models.py
@@ -1,10 +1,10 @@
-# System
+# Standard Library Imports
 import uuid
 
-# Django
+# Django Imports
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 
 class Wishlist(models.Model):
